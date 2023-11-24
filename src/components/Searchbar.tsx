6d19f5b880ade236d5885c3b95b4b48da9a0a1fc
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-11-24 10:11:54 +0530

    Added util function and scraped

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,21 +1,62 @@
 "use client";
 
-import React from "react";
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import React, { FormEvent, useState } from "react";
+
+const isValidAmazonProductUrl = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+};
 
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductUrl(searchPrompt);
+
+    if (!isValidLink) return alert("Please provide a valid Amazon link");
+
+    try {
+      setIsLoading(true);
+
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter product link ..."
         className="flex-1 min-w-[200px] w-full p-3 border border-gray-300 rounded-lg shadow-xs text-base text-gray-500 focus:outline-none"
       />
       <button
         type="submit"
-        className="bg-gray-900 border border-gray-900 rounded-lg shadow-xs px-5 py-3 text-white text-base font-semibold hover:opacity-90 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-40">
-        Search
+        disabled={searchPrompt === ""}
+        className="bg-gray-900 border border-gray-900 rounded-lg shadow-xs px-5 py-3 text-white text-base font-semibold hover:opacity-90 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-40"
+      >
+        {isLoading ? "Searching..." : "Search"}
       </button>
     </form>
   );
